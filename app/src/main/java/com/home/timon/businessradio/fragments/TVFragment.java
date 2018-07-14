19aeeea81c2c0c30d3e0ea5a2d9f427fee20e403
Taras Susulovskyi <timon9551@gmail.com>
2018-07-14 14:26:17 +0200

    Made TV fragment functionality just like radio fragment

--- app/src/main/java/com/home/timon/businessradio/fragments/TVFragment.java
+++ app/src/main/java/com/home/timon/businessradio/fragments/TVFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.home.timon.businessradio.MainActivity;
 import com.home.timon.businessradio.R;
 
 /**
@@ -16,13 +17,33 @@ import com.home.timon.businessradio.R;
 
 public class TVFragment extends Fragment {
 
+    private boolean paused;
+    private Button bt_play_pause;
+    private View viewFragment;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //just change the fragment_dashboard
-        //with the fragment you want to inflate
-        //like if the class is HomeFragment it should have R.layout.home_fragment
-        //if it is DashboardFragment it should have R.layout.fragment_dashboard
-        return inflater.inflate(R.layout.fragment_tv, null);
+        paused = true;
+        viewFragment = inflater.inflate(R.layout.fragment_tv, container, false);
+        bt_play_pause = viewFragment.findViewById(R.id.tv_button_play_pause);
+        bt_play_pause.setBackgroundResource(R.drawable.ic_baseline_play_arrow_24px);
+        bt_play_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.tv_button_play_pause:
+                        if (paused) {
+                            bt_play_pause.setBackgroundResource(R.drawable.ic_baseline_pause_24px);
+                            ((MainActivity)getActivity()).playRadio();
+                            paused = false;
+                        } else {
+                            bt_play_pause.setBackgroundResource(R.drawable.ic_baseline_play_arrow_24px);
+                            paused = true;
+                        }
+                }
+            }
+        });
+        return viewFragment;
     }
 }
